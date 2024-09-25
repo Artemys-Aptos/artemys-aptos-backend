203534e0025e3f0df206ff406861dcc5d2cb5237
mannyuncharted <omoebun52@gmail.com>
2024-09-25 12:46:25 +0100

    FEAT: updated connection pool

--- app/core/database.py
+++ app/core/database.py
@@ -14,8 +14,8 @@ Base = declarative_base()
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_size=100,  # Adjust based on your app's concurrency requirements
-    max_overflow=50,  # Allows for extra connections in times of high demand
+    pool_size=1000,  # Adjust based on your app's concurrency requirements
+    max_overflow=500,  # Allows for extra connections in times of high demand
     pool_timeout=120,  # Reduces wait time for a connection
     pool_recycle=36000,  # Recycles connections every 1 hours
     # echo_pool='debug',  # Logs pool checkouts/checkins (remove in production)
